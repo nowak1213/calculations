96ee8a3abd112e886148a3adf3cba25769c3b3cb
Bartlomiej Nowakowski <BartlomiejN1@verifone.com>
2020-08-05 21:56:00 +0200

    Initial commit of calculation project

---
+++ src/main/java/com/epam/nowakowski/bartlomiej/calculations/CalculationsApplication.java
@@ -0,0 +1,13 @@
+package com.epam.nowakowski.bartlomiej.calculations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculationsApplication.class, args);
+	}
+
+}
